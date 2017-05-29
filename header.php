d6d101c92c21d39300e379fa1fd5760535c917cd
Tim White <tim@whiteitsolutions.com.au>
2017-05-29 19:28:35 +1000

    Fix navbar

--- header.php
+++ header.php
@@ -15,11 +15,11 @@
         <div class="logo left"><img src="/wp-content/uploads/2016/10/Refugee_Connect_logo-wide_low.png" width="350"></div>
         <div class="nav right" style="text-align: right;">
             <ul>
-                <li><a href="#">HOME</a></li>
-                <li><a href="#about">ABOUT</a></li>
-                <li><a href="#donate">DONATE</a></li>
-                <li><a href="#workshops">WORKSHOPS</a></li>
-                <li><a href="#contact">CONTACT</a></li>
+                <li><a href="/#">HOME</a></li>
+                <li><a href="/#about">ABOUT</a></li>
+                <li><a href="/#donate">DONATE</a></li>
+                <li><a href="/#workshops">WORKSHOPS</a></li>
+                <li><a href="/#contact">CONTACT</a></li>
             </ul>
         </div>
         <div style="clear: both;"></div>
